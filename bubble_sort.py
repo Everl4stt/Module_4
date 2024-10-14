f5219c8ec1b20ad68ee13040a1eb367a48763544
Ivan <ciferu80@gmail.com>
2024-10-15 02:59:12 +0300

    commit

---
+++ bubble_sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(a) :
+    n = len(a)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+    return a
+a = [12, 2, 34, 5, 6, 9, 19, 4, 2]
+print("Отсортированный массив: ", bubble_sort(a))
