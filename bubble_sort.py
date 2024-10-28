bbed36bcd773ed1ad2597106acd7850ddffb9276
Ivan <ciferu80@gmail.com>
2024-10-28 20:50:37 +0300

    commit

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,3 +1,14 @@
+import time
+
+def func_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        print(f'Время на выполнение функции {func.__name__} = {time.time() - start_time} секунд')
+        return result
+    return wrapper
+
+@func_time
 def bubble_sort(a) :
     n = len(a)
     for i in range(n - 1):
@@ -5,5 +16,5 @@ def bubble_sort(a) :
             if a[j] > a[j + 1]:
                 a[j], a[j + 1] = a[j + 1], a[j]
     return a
-a = [12, 2, 34, 5, 6, 9, 19, 4, 2]
+a = [12, 2, 34, 5, 6, 9, 19, 4, 2, 23, 45, 56, 2, 23, 56, 67, 8, 10]
 print("Отсортированный массив: ", bubble_sort(a)) #Сложность алгоритма O(n^2)
--- merge_sort.py
+++ merge_sort.py
@@ -1,11 +1,24 @@
-def merge_sort(a):
-    if len(a) < 2:
-        return a[:]
+import time
+
+def func_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        print(f'Время на выполнение функции {func.__name__} = {time.time() - start_time} секунд')
+        return result
+    return wrapper
+
+@func_time
+def merge_sort(mass):
+    if len(mass) < 2:
+        return mass[:]
     else:
-        median = int(len(a)/2)
-        left = merge_sort(a[:median])
-        right = merge_sort(a[median:])
+        median = int(len(mass)/2)
+        left = merge_sort(mass[:median])
+        right = merge_sort(mass[median:])
         return merge(left, right)
+
+@func_time
 def merge(left, right):
     res = []
     i, j = 0, 0
@@ -23,5 +36,6 @@ def merge(left, right):
         res.append(right[j])
         j += 1
     return res
+
 a = [12, 2, 34, 5, 1, 45, 16, 2, 7, 4]
-print("Отсортированный массив: ", merge_sort(a)) #Сложность алгоритма O(n*log(n))
+print(f"Отсортированный массив: {merge_sort(a)}") #Сложность алгоритма O(n*log(n))
